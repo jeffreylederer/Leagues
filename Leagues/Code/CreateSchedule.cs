aec5484745db7497882d278151487fe6acfbe187
jeff <jeffrey@winnlederer.com>
2019-04-30 01:46:05 -0400

    fixed it for byes

--- Leagues/Code/CreateSchedule.cs
+++ Leagues/Code/CreateSchedule.cs
@@ -14,42 +14,98 @@ namespace Leagues.Code
         public List<Match> DoIt(int numberofWeeks, int numberOfTeams)
         {
 
-            numberOfTeams += numberOfTeams % 2;
-            var numberOfRinks = numberOfTeams / 2;
+            var Teams = numberOfTeams +  numberOfTeams % 2;
+            var numberOfRinks = Teams / 2;
+            bool bye = Teams != numberOfTeams;
 
             var matches = new List<Match>();
 
             int[] leftside = new int[numberOfRinks];
             int[] rightside = new int[numberOfRinks];
-            for (int r = 0; r < numberOfRinks; r++)
+            if (bye)
             {
-                leftside[r] = r;
-                rightside[r] = numberOfTeams - r - 1;
+                leftside[0] = 0;
+                rightside[0] = Teams - 1;
                 matches.Add(new Match()
                 {
                     Week = 0,
-                    Rink = r,
-                    Team1 = r,
-                    Team2 = numberOfTeams - r - 1
+                    Rink = -1,
+                    Team1 = 0,
+                    Team2 =  -1
                 });
+                for (int r = 1; r < numberOfRinks; r++)
+                {
+                    leftside[r] = r;
+                    rightside[r] = Teams - r - 1;
+                    matches.Add(new Match()
+                    {
+                        Week = 0,
+                        Rink = r - 1,
+                        Team1 = r,
+                        Team2 = Teams - r - 1
+                    });
+                }
             }
+            else
+            {
+                for (int r = 0; r < numberOfRinks; r++)
+                {
+                    leftside[r] = r;
+                    rightside[r] = Teams - r - 1;
+                    matches.Add(new Match()
+                    {
+                        Week = 0,
+                        Rink = r,
+                        Team1 = r,
+                        Team2 = Teams - r - 1
+                    });
+                }
+
+            }
+            
 
             for (int w = 1; w < numberofWeeks; w++)
             {
                 int remainder = ShiftRight(leftside);
                 int other = ShiftLeft(rightside, remainder);
                 leftside[1] = other;
+                int rink = 0;
                 for (int r = 0; r < numberOfRinks; r++)
                 {
-                    matches.Add(new Match()
+                    var left = leftside[r];
+                    var right = rightside[r];
+
+                    var match = new Match()
                     {
                         Week = w,
-                        Rink = r,
-                        Team1 = leftside[r]< rightside[r]? leftside[r]: rightside[r],
-                        Team2 = leftside[r] < rightside[r] ? rightside[r] : leftside[r]
-                    });
+                        Rink = rink,
+                        Team1 = left < right? left: right,
+                        Team2 = left < right ? right : left
+                    };
+                    if (bye && match.Team2 == Teams-1)
+                    {
+                        match.Team2 = -1;
+                        match.Rink = -1;
+                    }
+                    else
+                    {
+                        rink++;
+                    }
+                    matches.Add(match);
                 }
             }
+
+            // bye fixup
+            if (Teams != numberOfTeams)
+            {
+                for (int w = 1; w < numberofWeeks; w++)
+                {
+                    for (int r = 0; r < numberOfRinks; r++)
+                    {
+                    }
+                }
+            }
+            matches.Sort((a,b) => (a.Week*100 + a.Rink+1).CompareTo(b.Week*100+b.Rink+1));
             return matches;
 
         }
